79aba4d4082c2eccd4c015acc4967f0a41184d61
CamillaVieiraDevelopment <camillafranciannesrv@gmail.com>
2026-09-02 08:45:29 -0300

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
 import express from 'express';
 
 // Define the application environment
@@ -6,7 +8,18 @@ const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
 // Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
 
+// Define the current file and directory paths
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+
+/**
+ * Configure Express middleware
+ */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.send('Hello from Express!!!');
