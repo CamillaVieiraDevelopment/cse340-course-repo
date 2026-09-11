0a08dfa495a973adfd3c10963b4d50c4c9e31316
CamillaVieiraDevelopment <camillafranciannesrv@gmail.com>
2026-09-11 08:50:28 -0300

    initial comiit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import express from 'express';
 import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
+
 
 
 // Define the application environment
@@ -38,6 +40,9 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
+    console.log(organizations);
+
     const title = 'Our Partner Organizations';
     res.render('organizations', { title });
 });
