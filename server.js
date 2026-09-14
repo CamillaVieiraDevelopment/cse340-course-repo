9916b0dfbf3d55fb8d4320ddfa606d51b38c7df8
CamillaVieiraDevelopment <camillafranciannesrv@gmail.com>
2026-09-14 07:38:33 -0300

    initial commit

--- server.js
+++ server.js
@@ -20,9 +20,22 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-/**
- * Configure Express middleware
- */
+//**Configuretions Express middleware**
+
+// Middleware to log all incoming requests
+app.use((req, res, next) => {
+    if (NODE_ENV === 'development') {
+        console.log(`${req.method} ${req.url}`);
+    }
+    next(); // Pass control to the next middleware or route
+});
+
+// Middleware to make NODE_ENV available to all templates
+app.use((req, res, next) => {
+    res.locals.NODE_ENV = NODE_ENV;
+    next();
+});
+
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
